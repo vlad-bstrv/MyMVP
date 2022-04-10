8d82a2441bfcd47cee4c42749ed6f383b3d76b57
vlad-bstrv <keocera@icloud.com>
2022-04-10 11:50:51 +0300

    ДЗ2. Написал usecase для login

--- app/src/main/java/com/vladbstrv/mymvp/App.kt
+++ app/src/main/java/com/vladbstrv/mymvp/App.kt
@@ -2,14 +2,23 @@ package com.vladbstrv.mymvp
 
 import android.app.Application
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import com.vladbstrv.mymvp.data.LoginUsecaseImpl
 import com.vladbstrv.mymvp.data.MockLoginApiImpl
 import com.vladbstrv.mymvp.domain.LoginApi
+import com.vladbstrv.mymvp.domain.LoginUsecase
 
 class App : Application() {
 
-    val loginApi: LoginApi by lazy {
+    private val loginApi: LoginApi by lazy {
         MockLoginApiImpl()
     }
+
+    val loginUsecase: LoginUsecase by lazy {
+        LoginUsecaseImpl(app.loginApi, Handler(Looper.getMainLooper()))
+    }
+
 }
 
 val Context.app: App
--- app/src/main/java/com/vladbstrv/mymvp/ui/login/LoginPresenter.kt
+++ app/src/main/java/com/vladbstrv/mymvp/ui/login/LoginPresenter.kt
@@ -4,13 +4,12 @@ import android.os.Handler
 import android.os.Looper
 import com.vladbstrv.mymvp.data.MockLoginApiImpl
 import com.vladbstrv.mymvp.domain.LoginApi
+import com.vladbstrv.mymvp.domain.LoginUsecase
 import java.lang.Thread.sleep
 
-class LoginPresenter(private val api: LoginApi) :  LoginContract.Presenter{
+class LoginPresenter(private val loginUsecase: LoginUsecase) :  LoginContract.Presenter{
 
     private lateinit var view: LoginContract.View
-    private val uiHandler = Handler(Looper.getMainLooper())
-
 
     override fun onAttach(view: LoginContract.View) {
         this.view = view
@@ -19,17 +18,16 @@ class LoginPresenter(private val api: LoginApi) :  LoginContract.Presenter{
     override fun onLogin(login: String, password: String) {
         view.showProgress()
 
-        Thread {
-            val success = api.login(login, password)
-            uiHandler.post {
-                view.hideProgress()
-                if (success) {
-                    view.setSuccess()
-                } else {
-                    view.setError()
-                }
+        loginUsecase.login(login, password) { result ->
+            view.hideProgress()
+            if (result) {
+                view.setSuccess()
+            } else {
+                view.setError()
             }
-        }.start()
+        }
+
+
     }
 
 
